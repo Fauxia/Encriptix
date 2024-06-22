121d208e9a09c517417f4ad5ec57517c7521e4be
Husnain Ali <fauxia39@gmail.com>
2024-06-22 08:50:47 -0700

    Calculator

---
+++ Calculator/app.js
@@ -0,0 +1,35 @@
+let input = document.querySelector(".in")
+let btns = document.querySelectorAll("button")
+
+let arr = Array.from(btns)
+console.log(arr);
+
+let string = "0"
+input.value = string
+
+arr.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        if (e.target.textContent == "AC") {
+            string = "0"
+            input.value = string
+        } else if(e.target.textContent == "DE"){
+            string = string.substring(0,string.length -1)
+            input.value = string
+            console.log(input.value);
+        }else if(e.target.textContent == "="){
+            string = eval(string)
+            input.value = string
+        }
+        else{
+            if(input.value == "0"){
+                string = e.target.textContent
+                input.value = string
+             }else{
+                string += e.target.textContent
+                input.value = string
+                
+            }
+
+        }
+    })
+})
